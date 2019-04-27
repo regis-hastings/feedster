e33f6e176bf5999957915132391b31946adb4eb4
Regis <25518602+rfjues@users.noreply.github.com>
2019-04-27 10:14:52 -0700

    Added hover functionality to nav menu and buttons

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 $(document).ready(() => {
-
+  $('.menu').on('mouseenter', () => {
+    $('.nav-menu').show();
+  });
+  $('.nav-menu').on('mouseleave', () => {
+    $('.nav-menu').hide();
+  });
+  $('.btn').on('mouseenter', event => {
+    $(event.currentTarget).addClass('btn-hover');
+  }).on('mouseleave', event => {
+    $(event.currentTarget).removeClass('btn-hover');
+  });
 }); 
