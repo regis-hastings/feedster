bf386e4ef7ae0af99c4b6d219abd9a3a83f92d67
Regis <25518602+rfjues@users.noreply.github.com>
2019-04-27 10:42:26 -0700

    added limit user post to 140 characters feature

--- main.js
+++ main.js
@@ -10,4 +10,15 @@ $(document).ready(() => {
   }).on('mouseleave', event => {
     $(event.currentTarget).removeClass('btn-hover');
   });
+  $('.postText').on('keyup', event => {
+    const post = $(event.currentTarget).val();
+    let remaining = 140 - post.length;
+    if (remaining <= 0) {
+      $('.wordcount').addClass('red');
+    } else {
+      $('.wordcount').removeClass('red');
+    }
+    $('.characters').html(remaining);
+  });
+  $('.postText').focus();
 }); 
